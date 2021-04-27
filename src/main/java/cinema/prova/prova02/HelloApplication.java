46718f83c5dc93d1948c2543002c2c00b6874eb8
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-04-27 20:34:39 -0300

    primeiro commit

---
+++ src/main/java/cinema/prova/prova02/HelloApplication.java
@@ -0,0 +1,9 @@
+package cinema.prova.prova02;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
